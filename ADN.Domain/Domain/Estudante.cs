2d8cd6d66e28bbae311019b14281abfe744b7170
André Mendes Marcondes <andre.marcondes@betalearning.com.br>
2022-09-05 18:11:42 -0300

    Criando dominio

---
+++ ADN.Domain/Domain/Estudante.cs
@@ -0,0 +1,10 @@
+﻿namespace ADN.Domain.Domain
+{
+    public class Estudante
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
