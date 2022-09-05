c748d8b4c66045a19229e82b2e9691a211e9c514
André Mendes Marcondes <andre.marcondes@betalearning.com.br>
2022-09-05 18:26:48 -0300

    criando service

---
+++ ADN.Domain/Interfaces/Services/IEstudanteService.cs
@@ -0,0 +1,9 @@
+﻿using ADN.Domain.Domain;
+
+namespace ADN.Domain.Interfaces.Services
+{
+    public interface IEstudanteService
+    {
+        Task<List<Estudante>> GetAll();
+    }
+}
---
+++ ADN.Service/Services/EstudanteService.cs
@@ -0,0 +1,21 @@
+﻿using ADN.Domain.Domain;
+using ADN.Domain.Interfaces.Repositorio;
+using ADN.Domain.Interfaces.Services;
+
+namespace ADN.Service.Services
+{
+    public class EstudanteService : IEstudanteService
+    {
+        private readonly IEstudanteRepositorio _repositorio;
+
+        public EstudanteService(IEstudanteRepositorio repositorio)
+        {
+            _repositorio = repositorio;
+        }
+
+        public async Task<List<Estudante>> GetAll()
+        {
+            return await _repositorio.GetAll();
+        }
+    }
+}
